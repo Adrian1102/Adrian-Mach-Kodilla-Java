2d93f5bac3ab51d0eb94949bdfa0e215e45be597
unknown <300adrianmach@gmail.com>
2023-09-21 22:41:56 +0200

    First Commit

---
+++ src/main/java/com/kodilla/KodillaCourse1337Application.java
@@ -0,0 +1,13 @@
+package com.kodilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaCourse1337Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaCourse1337Application.class, args);
+    }
+
+}
